3dcca0f8115879b88e4c61ca046198245424da24
eya eloued <eloued.eya98@gmail.com>
2024-01-28 15:11:13 +0100

    ajouter une bare de recherche

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,19 +1,39 @@
 import styles from "./Content.module.scss";
 import Recipe from "./Recipe";
-import { data } from "../data/recipes"
+import { data } from "../data/recipes";
+import { useState } from "react";
 
 function Content() {
   const recipes = data;
+  const [filter, setFilter] = useState("");
+
+  function handelInput(e) {
+
+    const filter = e.target.value;
+
+    setFilter(filter.trim().toLowerCase());
+  }
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30">Découvrez nos nouvelles recettes </h1>
       <div className={`card p-20 ${styles.contentCard}`}>
+        <div
+          className={`d-flex flex-row justify-content-center align-item-center my-30 ${styles.searchBar}`}
+        >
+          <i className="fa-solid fa-magnifying-glass mr-15"></i>
+          <input
+            type="text"
+            onInput={handelInput}
+            className="flex-fill"
+            placeholder="Recherche"
+          />
+        </div>
         <div className={styles.grid}>
-          {recipes.map((r)=>(
-            <Recipe key={r._id} title={r.title} image={r.image} />
-            
-          ))}
-
+          {recipes
+            .filter((r) => r.title.toLowerCase().startsWith(filter))
+            .map((r) => (
+              <Recipe key={r._id} title={r.title} image={r.image} />
+            ))}
         </div>
       </div>
     </div>
