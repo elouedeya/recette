98d48d9374cd0a125faba2b19508fa764fcc77e2
eya eloued <eloued.eya98@gmail.com>
2024-01-28 14:41:44 +0100

    ajouter une bare de menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,34 @@
-import styles from "./Header.module.scss"
-import recetteTr from "../assets/images/recetteTr.png"
-
+import styles from "./Header.module.scss";
+import recetteTr from "../assets/images/recetteTr.png";
+import { useState } from "react";
+import HeaderMenu from "./HeaderMenu";
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
   return (
-  <header className={`${styles.header} d-flex flex-row align-items-center`}>
-    <i className="fa-solid fa-bars mr-15"></i>
-    <div className="flex-fill">
-      <img src={recetteTr} alt="logo recette" />
-    </div>
-    <ul>
-      <button className="mr-5 btn btn-reverse-primary">
-      <i className="fa-solid fa-basket-shopping mr-5"></i>
-      <span>panier</span>
-      </button>
-      <button className="btn btn-primary">connexion</button>
-    </ul>
-  </header>
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
+      
+      <div className="flex-fill">
+        <img src={recetteTr} alt="logo recette" />
+      </div>
+      <ul className={styles.headerList}>
+        <button className="mr-5 btn btn-reverse-primary">
+          <i className="fa-solid fa-basket-shopping mr-5"></i>
+          <span>Wishlist</span>
+        </button>
+        <button className="btn btn-primary">connexion</button>
+      </ul>
+      <i
+        onClick={() => setShowMenu(true)}
+        className={`fa-solid fa-bars ${styles.headerXs}`}
+      ></i>
+      {showMenu && (
+        <>
+          <div className="calc" onClick={() => setShowMenu(false)}></div>
+          <HeaderMenu />
+        </>
+      )}
+    </header>
   );
 }
 
